37b8651390e654a0fce661f7cb272a2e3c99a54b
Sofija Durward <57948395+sofi-dev@users.noreply.github.com>
2023-10-24 18:08:59 +0100

    working test of tmdbsimple

--- tmdb.py
+++ tmdb.py
@@ -1,3 +1,9 @@
 import tmdbsimple as tmdb
 import numpy as np
 import pandas as pd
+
+tmdb.API_KEY = '78b74aad78b8672c26d0fb8596c37841'
+
+movie = tmdb.Movies(100)
+response = movie.info()
+print(movie.title)
